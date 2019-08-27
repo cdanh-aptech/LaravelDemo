77e81eff51b18f458fd8e1b91448a97ae79bfc83
cdanh-aptech <cdanha18037@cusc.ctu.edu.vn>
2019-08-27 20:53:32 +0700

    Create project

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,16 +5,12 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Run the database seeds.
+     * Seed the application's database.
      *
      * @return void
      */
     public function run()
     {
-        //$this->call(UsersTableSeeder::class);
-        //$this->call(TheLoaiTableSeeder::class);
-        //$this->call(LoaiTinTableSeeder::class);
-        //$this->call(TinTucTableSeeder::class);
-        $this->call(CommentTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
     }
 }
