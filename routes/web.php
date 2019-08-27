715ef69d0d8d97f3a091f83626c5fce17385c0be
cdanh-aptech <cdanha18037@cusc.ctu.edu.vn>
2019-08-28 00:54:56 +0700

    Admin template

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+use App\TheLoai;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('thu', function () {
+    return view('admin.theloai.danhsach');
+});
+
+
